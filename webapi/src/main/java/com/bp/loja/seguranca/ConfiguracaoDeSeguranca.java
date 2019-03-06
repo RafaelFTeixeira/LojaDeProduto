64ff514dccfa4b1e11eca75870f2ad505bbd3a9b
Rafael Teixeira <rafaelfteixeira@hotmail.com>
2019-03-06 00:58:44 -0300

    liberando documentacao de api

--- webapi/src/main/java/com/bp/loja/seguranca/ConfiguracaoDeSeguranca.java
+++ webapi/src/main/java/com/bp/loja/seguranca/ConfiguracaoDeSeguranca.java
@@ -14,11 +14,23 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Profile("producao")
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+  private static final String[] LISTA_DE_AUTORIZACAO = {
+    "/swagger-resources",
+    "/swagger-resources/**",
+    "/swagger-ui.html",
+    "/v2/api-docs",
+    "/h2-console/**",
+    "/configuration/ui",
+    "/configuration/security",
+    "/webjars/**"
+};
 
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll()
-        .antMatchers("/", "/h2-console/**").permitAll().anyRequest().authenticated().and()
+    httpSecurity.csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .antMatchers(LISTA_DE_AUTORIZACAO).permitAll()
+        .anyRequest().authenticated().and()
         .addFilterBefore(new LoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
         .addFilterBefore(new AutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
 
